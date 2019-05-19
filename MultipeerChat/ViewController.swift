e2eb590e8565edd81689bd930d353f05d722a000
Muche <iskandre.muchery@epitech.eu>
2019-05-19 17:12:08 +0200

    Initial Commit

---
+++ MultipeerChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MultipeerChat
+//
+//  Created by Iskandre Muchery on 19/05/2019.
+//  Copyright © 2019 Iskandre Muchery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
